e08bae928b9c760ca869dc0f74ea46ac2f43e92f
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-05-30 20:10:29 +0900

    opencv-contrib,numpy,cmake added

--- v0.8.4/numpy120x/mediapipe-0.8-cp37-none-linux_aarch64_numpy120x_download.sh
+++ v0.8.4/numpy120x/mediapipe-0.8-cp37-none-linux_aarch64_numpy120x_download.sh
@@ -14,6 +14,6 @@ curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE
 
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1qrAtG7FB3SFUKzll817d4P_tuzSQCItE" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1qrAtG7FB3SFUKzll817d4P_tuzSQCItE" -o opencv_contrib_python-4.5.2.52-cp37-cp37m-linux_aarch64.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1qrAtG7FB3SFUKzll817d4P_tuzSQCItE" -o opencv_contrib_python-4.5.2.52-cp37-none-linux_aarch64.whl
 
 echo Download finished.
