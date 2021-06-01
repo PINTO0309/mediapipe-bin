a681ad1d3478715c664e95ad1178e88505e59a71
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-06-01 23:17:57 +0900

    jetson nano, L4T32.5.1

---
+++ v0.8.4/numpy119x/mediapipe-0.8-cp36-none-linux_aarch64_numpy119x_jetsonnano_L4T32.5.1_download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=15JN3sYMuKsV1AVmNHRFlN5pQ1LVh8MVa" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=15JN3sYMuKsV1AVmNHRFlN5pQ1LVh8MVa" -o numpy-1.19.4-cp36-none-manylinux2014_aarch64.whl
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1M7LdJjHZoz648KnZreKZawRmWK3UD3ib" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1M7LdJjHZoz648KnZreKZawRmWK3UD3ib" -o opencv_contrib_python-4.5.2.52-cp36-none-linux_aarch64.whl
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1ZNh4omIMQwS8RFc8289Yk29cNRNCkkdZ" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1ZNh4omIMQwS8RFc8289Yk29cNRNCkkdZ" -o mediapipe-0.8.4_cuda102-cp36-none-linux_aarch64.whl
+
+echo Download finished.
