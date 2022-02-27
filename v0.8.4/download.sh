d4fd69d5ef9c7b25b268e653114f1bb7cced10b6
pinto0309 <rmsdh122@yahoo.co.jp>
2022-02-27 22:36:47 +0900

    Google Drive -> Releases

---
+++ v0.8.4/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -OL https://github.com/PINTO0309/mediapipe-bin/releases/download/v0.8.4/v0.8.4.zip
+
+echo Download finished.
---
+++ v0.8.5/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -OL https://github.com/PINTO0309/mediapipe-bin/releases/download/v0.8.5/v0.8.5.zip
+
+echo Download finished.
